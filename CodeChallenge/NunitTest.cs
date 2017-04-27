cee90621c5ae5bc073d3374ee2fc5ee3418be7dd
Artem Aleksandruk <Artem Aleksandruk>
2017-04-27 11:54:35 -0400

    my first commit on windows

---
+++ CodeChallenge/NunitTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+// Installed NUnit from the site. Added the NUnit.dll into preferences of the project
+// and installed a extension to use it inside visual studio.
+
+namespace CodeChallenge
+{
+    [TestFixture]
+
+    public class NunitTest
+    {
+        [Test]
+
+        
+        // A sample test that is working.
+        public void testingSample()
+        {
+            int a = 100;
+            int b = 100;
+            Assert.AreEqual(a, b);
+        }
+
+   // At the bootcamp we covered TDD but had very little practice due to time. We never wrote our own test, 
+   // We just had homework assignements where we fixed tests given to use. I am at a loss where to start writing my test.
+   // I understand the logic but have no clue were to begin.
+
+        public void filePathTest()
+        {
+            // I would like to test to see if a filePath is a string inputed from user.
+        }
+
+        public void counterTest()
+        {
+            // I would like to make sure that the counter is working properly.
+        }
+
+        public void testList()
+        {
+            // I would like to make sure the list is growing and that the .Add() working.
+            // I would check the list.length and compare it from before.
+        }
+
+        public void testPrimeLogic()
+        {
+            // Here I would test the prime logic and see if the remainder is working correctly.
+            // Also would like to make sure that if a string comes into the logic that it catches it.
+        }
+    }
+}
---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge
+{
+    class PrimeChallenge
+    {
+        static void Main(string[] args)
+        {
+            // Define variables
+            string filePath;
+            int counter = 0;
+            string line;
+
+            // Command-line arugment to enter a file containing one integer per line.
+            Console.WriteLine("Please enter file path: ");
+            filePath = Console.ReadLine();
+ 
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(filePath);
+            while ((line = file.ReadLine()) != null)
+            {
+                // Need to turn the string from ReadlLine() into a integer.
+                int number = int.Parse(line);
+                var primeList = primesLogic(number);
+
+            // Needed to print out the values in the list and not the list object. Found a solution using string.Join().
+                Console.WriteLine("{0}: {1}", line, string.Join(", ",primeList));
+                counter++;
+            }
+            file.Close();
+            //Suspend the screen.
+            Console.ReadLine();
+
+            
+        }
+
+        /// <summary>
+        /// Reads a number and finds the primes of number.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static List<int> primesLogic(int number)
+        {
+            var primeNums = new List<int>();
+
+            // Logic to find prime numbers and then used a .Add() to add it to the new List.
+            for (int i = 2; i <= number; i++)
+            {
+                while (number % i == 0)
+                {
+                    primeNums.Add(i);
+                    number = number / i;
+                }
+            }
+            return primeNums;
+        }
+    }
+}
